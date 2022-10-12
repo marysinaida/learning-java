e0ccde90c9d89ecae233d800a10e7b7395efcf0e
schneider <marydorcassinaida54@gmail.com>
2022-10-12 14:45:26 +0300

    learning java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main{
+    public static void main(String args[]){
+        
+       int examScore = 90;
+       double dexamScore = examScore;
+       int intExamScore = (int) dexamScore;
+       /*float floatNumber = 5.5566f;
+       boolean hasPass = true;
+       String name = "Mae Dee";
+        */
+
+       Test.displayResult();
+    
+   System.out.println("Hello world!" + String.valueOf(intExamScore));
+    
+
+    }
+
+}
---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test{
+    public static void displayResult(){
+        System.out.println("am testing");
+        
+    }
+}
