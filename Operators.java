d82a01a86f5cb5261e61e4ddeb7e26a9daa900cd
schneider <marydorcassinaida54@gmail.com>
2022-10-14 12:36:28 +0300

    added condition statement samples

--- Operators.java
+++ Operators.java
@@ -1,7 +1,21 @@
+import java.util.function.IntFunction;
+
 public class Operators {
     public static void main(String[] args) {
         int valueA = 10;
         int valueB = 20;
-        System.out.println("Your Result: " + (valueB % valueA));
+        int examScore = 80;
+        valueB = 2; 
+        valueB += 33; // or else valueB = valueB + 33;
+        valueB -= 33; // or else valueB = valueB + 33;
+
+        if (examScore >= 70){
+
+        System.out.println("Passed with A");
+        }else if (examScore >= 60){
+            System.out.println("Passed with B");
+        }else{
+            System.out.println("Failed");
+        }
     }
 }
