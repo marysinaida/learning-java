6fe2a595dd90f34a629bd2dce0314e1a80f364b3
schneider <marydorcassinaida54@gmail.com>
2022-10-14 14:02:13 +0300

    added a modulus of odd numbers

--- Iteration.java
+++ Iteration.java
@@ -1,7 +1,7 @@
 public class Iteration {
     public static void main(String[] args) {
-        for (int index = 1; index < 6; index++) {
-         if  (index % 2 == 0){
+        for (int index = 1; index < 21; index++) {
+         if  (index % 2 == 1){
             System.out.println("index " + index);
         }
         }
