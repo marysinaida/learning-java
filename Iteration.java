c94ae00ea6e0067db3dff89cb92543001f4471ba
schneider <marydorcassinaida54@gmail.com>
2022-10-14 13:57:26 +0300

    added iteration sample code

--- Iteration.java
+++ Iteration.java
@@ -1,3 +1,9 @@
 public class Iteration {
-    
+    public static void main(String[] args) {
+        for (int index = 1; index < 6; index++) {
+         if  (index % 2 == 0){
+            System.out.println("index " + index);
+        }
+        }
+    }
 }
